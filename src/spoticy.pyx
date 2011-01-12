509c57c2a004656eb2dac76d3f7239d1db30d689
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-01-12 22:22:44 +0100

    Remove _c prefix from internal C variables

--- src/spoticy.pyx
+++ src/spoticy.pyx
@@ -94,25 +94,25 @@ cdef void logged_in_callback(libspotify.sp_session* sp_session,
     if userdata is not NULL:
         session_callbacks = <object> userdata
         session = Session()
-        session._c_session = sp_session
+        session._session = sp_session
         session_callbacks.logged_in(session_callbacks, session, sp_error)
 
 
 ### Session config
 
 cdef class SessionConfig(object):
-    cdef libspotify.sp_session_callbacks* _c_callbacks
-    cdef libspotify.sp_session_config _c_config
+    cdef libspotify.sp_session_callbacks* _callbacks
+    cdef libspotify.sp_session_config _config
     cdef bytes _application_key
     cdef object _session_callbacks
 
     def __cinit__(self):
-        self._c_callbacks = <libspotify.sp_session_callbacks*> \
+        self._callbacks = <libspotify.sp_session_callbacks*> \
             stdlib.malloc(sizeof(libspotify.sp_session_callbacks))
 
     def __dealloc__(self):
-        if self._c_config.callbacks is not NULL:
-            stdlib.free(self._c_callbacks)
+        if self._config.callbacks is not NULL:
+            stdlib.free(self._callbacks)
 
     def __init__(self, bytes application_key, object session_callbacks,
             unicode cache_location=None, unicode settings_location=None,
@@ -125,100 +125,100 @@ cdef class SessionConfig(object):
         self._application_key = application_key
         self._session_callbacks = session_callbacks
 
-        self._c_config.api_version = libspotify.SPOTIFY_API_VERSION
+        self._config.api_version = libspotify.SPOTIFY_API_VERSION
 
         if cache_location is None:
             cache_location = u''
         cache_location_str = to_string(cache_location)
-        self._c_config.cache_location = cache_location_str
+        self._config.cache_location = cache_location_str
 
         if settings_location is None:
             settings_location = u'tmp'
         settings_location_str = to_string(settings_location)
-        self._c_config.settings_location = settings_location_str
+        self._config.settings_location = settings_location_str
 
-        self._c_config.application_key = <void*>(<char*> self._application_key)
-        self._c_config.application_key_size = len(self._application_key)
+        self._config.application_key = <void*>(<char*> self._application_key)
+        self._config.application_key_size = len(self._application_key)
 
         if user_agent is None:
             user_agent = u'Spoticy'
         user_agent_str = to_string(user_agent)
-        self._c_config.user_agent = user_agent_str
+        self._config.user_agent = user_agent_str
 
-        self._c_callbacks.logged_in = logged_in_callback
+        self._callbacks.logged_in = logged_in_callback
         # TODO Assign pointer to callback struct to config struct
 
-        self._c_config.userdata = <void*> self._session_callbacks
+        self._config.userdata = <void*> self._session_callbacks
 
-        self._c_config.tiny_settings = tiny_settings
+        self._config.tiny_settings = tiny_settings
 
     property api_version:
         def __get__(self):
-            return self._c_config.api_version
+            return self._config.api_version
 
     property cache_location:
         def __get__(self):
-            return to_unicode(<char*> self._c_config.cache_location)
+            return to_unicode(<char*> self._config.cache_location)
 
     property settings_location:
         def __get__(self):
-            return to_unicode(<char*> self._c_config.settings_location)
+            return to_unicode(<char*> self._config.settings_location)
 
     property application_key:
         def __get__(self):
-            return <char*> self._c_config.application_key
+            return <char*> self._config.application_key
 
     property application_key_size:
         def __get__(self):
-            return self._c_config.application_key_size
+            return self._config.application_key_size
 
     property user_agent:
         def __get__(self):
-            return to_unicode(<char*> self._c_config.user_agent)
+            return to_unicode(<char*> self._config.user_agent)
 
     property userdata:
         def __get__(self):
-            return <object> self._c_config.userdata
+            return <object> self._config.userdata
 
     property tiny_settings:
         def __get__(self):
-            return self._c_config.tiny_settings
+            return self._config.tiny_settings
 
 
 ### Session handling
 
 cdef class Session(object):
-    cdef libspotify.sp_session* _c_session
+    cdef libspotify.sp_session* _session
 
     def __init__(self, SessionConfig config=None):
         if config is not None:
             is_ok(libspotify.sp_session_create(
-                &config._c_config, &self._c_session))
+                &config._config, &self._session))
         else:
-            pass # Assuming that you are setting _c_session directly
+            pass # Assuming that you are setting _session directly
             # XXX Would be nice to be able to get a sp_session* as an argument
             # and set it before the end of __init__, so we're never in an
             # invalid state.
 
     def release(self):
-        if self._c_session is not NULL:
-            libspotify.sp_session_release(self._c_session)
-            self._c_session = NULL
+        if self._session is not NULL:
+            libspotify.sp_session_release(self._session)
+            self._session = NULL
 
     property connection_state:
         def __get__(self):
-            if self._c_session is not NULL:
-                return libspotify.sp_session_connectionstate(self._c_session)
+            if self._session is not NULL:
+                return libspotify.sp_session_connectionstate(self._session)
 
     def login(self, unicode username, unicode password):
-        if self._c_session is NULL:
+        if self._session is NULL:
             raise Exception(u'Session not initialized')
         username_str = to_string(username)
         password_str = to_string(password)
         is_ok(libspotify.sp_session_login(
-            self._c_session, username_str, password_str))
+            self._session, username_str, password_str))
 
     def logout(self):
-        if self._c_session is NULL:
+        if self._session is NULL:
             raise Exception(u'Session not initialized')
-        is_ok(libspotify.sp_session_logout(self._c_session))
+        is_ok(libspotify.sp_session_logout(self._session))
