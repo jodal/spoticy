e96344da36b3916c92065ed2383f543c977ba932
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-01-15 22:53:26 +0100

    Remove redundant session creation from tests

--- tests/session_test.py
+++ tests/session_test.py
@@ -8,14 +8,13 @@ class SessionTest(unittest.TestCase):
         application_key = open('tests/spotify_appkey.key').read()
         callbacks = spoticy.SessionCallbacks()
         self.config = spoticy.SessionConfig(application_key, callbacks)
-        self.session = None
+        self.session = spoticy.Session(self.config)
 
     def tearDown(self):
         if self.session is not None:
             self.session.release()
 
     def test_create_session_with_valid_config_should_succeed(self):
-        self.session = spoticy.Session(self.config)
         self.assertEqual(0, self.session.connection_state)
 
     def test_session_without_config_should_not_be_usable(self):
@@ -27,7 +26,6 @@ class SessionTest(unittest.TestCase):
         self.assertRaises(Exception, self.session.process_events)
 
     def test_release_should_not_raise_exceptions(self):
-        self.session = spoticy.Session(self.config)
         self.session.release()
         self.assertEqual(None, self.session.connection_state)
         self.assertRaises(Exception, self.session.login,
@@ -36,20 +34,16 @@ class SessionTest(unittest.TestCase):
         self.assertRaises(Exception, self.session.process_events)
 
     def test_process_events_should_return_ms_to_wait_before_next_call(self):
-        self.session = spoticy.Session(self.config)
         result = self.session.process_events()
         self.assert_(result > 0)
 
     def test_cache_size_should_be_settable_to_a_number_of_megabytes(self):
-        self.session = spoticy.Session(self.config)
         self.session.cache_size_in_mb = 100
 
     def test_cache_size_should_be_settable_to_ten_percent_of_free_disk(self):
-        self.session = spoticy.Session(self.config)
         self.session.cache_size_in_mb = 0 # == 10% of free disk
 
     def test_cache_size_should_not_be_readable(self):
-        self.session = spoticy.Session(self.config)
         try:
             self.session.cache_size_in_mb
             self.fail(u'Cache size should not be readable')
