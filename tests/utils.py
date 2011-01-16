94fc9ccd28e71211449f00955d30c187d9baadeb
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-01-16 22:50:39 +0100

    Add more callback logging during tests

--- tests/utils.py
+++ tests/utils.py
@@ -15,11 +15,12 @@ playlists_loaded_event = threading.Event()
 
 def wait_for_event(session, event):
     while not event.is_set():
-        awoken.clear()
         timeout = session.process_events()
         timer = threading.Timer(timeout / 1000., awoken.set)
         timer.start()
+        log('Waiting for events', timeout=timeout)
         awoken.wait()
+        awoken.clear()
         timer.cancel()
     event.clear()
 
@@ -34,12 +35,39 @@ class TestSessionCallbacks(spoticy.SessionCallbacks):
         logged_out_event.set()
         awoken.set()
 
+    def metadata_updated(self, session):
+        log('metadata_updated called')
+
     def connection_error(self, session, error):
         log('connection_error called', error=error)
+
+    def message_to_user(self, session, message):
+        log('message_to_user called', msg=message)
 
     def notify_main_thread(self, session):
         log('notify_main_thread called')
         awoken.set()
+
+    def music_delivery(self, session, sample_type, sample_rate, channels,
+            frames, num_frames):
+        log('music_delivery called', sample_type=sample_type,
+            sample_rate=sample_rate, channels=channels, num_frames=num_frames)
+
+    def play_token_lost(self, session):
+        log('play_token_lost called')
+
+    def log_message(self, session, message):
+        log('log_message called', msg=message)
+
+    def end_of_track(self, session):
+        log('end_of_track called')
+
+    def streaming_error(self, session, error):
+        log('streaming_error called', error=error)
+
+    def userinfo_updated(self, session):
+        log('userinfo_updated called')
+
 
 class TestPlaylistsCallbacks(spoticy.PlaylistsCallbacks):
     def playlist_added(self, playlists, added_playlist, added_position):
